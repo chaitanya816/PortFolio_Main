e8d2133aa30c13e1aa9a8a4645dcb251ff5ecaf8
chaitanya816 <160163592+chaitanya816@users.noreply.github.com>
2024-04-08 10:24:21 +0000

    hoverplay

--- script.js
+++ script.js
@@ -67,3 +67,51 @@ menuIcon.addEventListener("click", () => {
 
 
 })
+
+
+let clip = document.querySelector(".Netflixvdo") 
+  
+
+clip.addEventListener("mouseover", function (e) { 
+    clip.play(); 
+}) 
+
+clip.addEventListener("mouseout", function (e) { 
+    clip.pause(); 
+}) 
+
+
+let clip1 = document.querySelector(".domvdo") 
+  
+
+clip1.addEventListener("mouseover", function (e) { 
+    clip1.play(); 
+}) 
+
+clip1.addEventListener("mouseout", function (e) { 
+    clip1.pause(); 
+}) 
+
+let clip2 = document.querySelector(".codecrafthub") 
+  
+
+clip2.addEventListener("mouseover", function (e) { 
+    clip2.play(); 
+}) 
+
+clip2.addEventListener("mouseout", function (e) { 
+    clip2.pause(); 
+}) 
+
+
+let clip3 = document.querySelector(".paytmvdo") 
+  
+
+clip3.addEventListener("mouseover", function (e) { 
+    clip3.play(); 
+}) 
+
+clip3.addEventListener("mouseout", function (e) { 
+    clip3.pause(); 
+}) 
+
